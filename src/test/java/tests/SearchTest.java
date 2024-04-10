27da8de856733d70cfab280997cd04a1f9745835
k.nemov <k.nemov@bftcom.com>
2024-04-10 23:26:54 +0300

    add allure reports

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 //Проверка результатов поисковой выдачи на наличие конкретной записи
 public class SearchTest extends BaseTest {
     @Test
+    @DisplayName("Test search result")
     public void searchSiteTest() {
         testMethods.openPage(driver, googleMainPage.URL);
         testMethods.textInputAndEnter("тест");
